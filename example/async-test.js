5c49bf95083c3b0e41da28fb4de90b4d0b5e6673
Anastasiia Linchevska <linch3500@gmail.com>
2026-04-24 17:41:36 +0300

    added demo cases for async/await and promise version

--- example/async-test.js
+++ example/async-test.js
@@ -16,3 +16,29 @@ asyncMapCallback(orderIds, fetchOrderCallback, (err, results) => {
     if (err) console.error("Помилка:", err);
     else console.log("✅ Результат Callbacks:", results);
 });
+// --- ДЕМО 2 & 3: Promise-версія і Async/Await ---
+
+const fetchOrderPromise = (id, signal) => {
+    return new Promise((resolve, reject) => {
+        const timeoutId = setTimeout(() => resolve(`Деталі замовлення #${id}`), 1000);
+        
+        // якщо був сигнал відміни то очищаем таймаут
+        if (signal) {
+            signal.addEventListener('abort', () => {
+                clearTimeout(timeoutId);
+                reject(new Error("Перервано користувачем (AbortError)"));
+            });
+        }
+    });
+};
+
+async function runAsyncAwaitDemo() {
+    console.log("\n2. Тест Async/Await версії (завантаження...)");
+    try {
+        const results = await asyncMapPromise(orderIds, fetchOrderPromise);
+        console.log("✅ Результат Async/Await:", results);
+    } catch (error) {
+        console.error("Помилка:", error);
+    }
+}
+setTimeout(runAsyncAwaitDemo, 1000); // Запускаемо пізніше щоб не змішати вивід з Callback-версією
